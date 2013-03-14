6ead6d99ee01754e842137a85383ed358d6f4d89
Kevin Ciminato <Travail@172.20.202.134>
2013-03-14 15:34:56 +0100

    Ajout de la vue accueil à inclure après la vue sommaire à la connexion

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,37 +1,52 @@
 ﻿<?php
-if(!isset($_REQUEST['action'])){
-	$_REQUEST['action'] = 'demandeConnexion';
+if(!isset($_REQUEST['action']))
+{
+    $_REQUEST['action'] = 'demandeConnexion';
 }
 $action = $_REQUEST['action'];
-switch($action){
-	case 'demandeConnexion':{
-		include("vues/v_connexion.php");
-		break;
-	}
-	case 'valideConnexion':{
-		$login = $_REQUEST['login'];
-		$mdp = $_REQUEST['mdp'];
-		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
-		if(!is_array( $visiteur)){
-			ajouterErreur("Login ou mot de passe incorrect");
-			include("vues/v_erreurs.php");
-			include("vues/v_connexion.php");
-		}
-		else{
-			$id = $visiteur['VIS_MATRICULE'];
-			$nom =  $visiteur['VIS_NOM'];
-			$prenom = $visiteur['VIS_PRENOM'];
-                        $_SESSION['login']= $login; // mémorise les variables de session
-			$_SESSION['vis_matricule']= $id;
-                        $_SESSION['nom']= $nom;
-                        $_SESSION['prenom']= $prenom;
-                        include("vues/v_sommaire.php");
-			}
-		break;
-	}
-	default :{
-		include("vues/v_connexion.php");
-		break;
-	}
+switch($action)
+{
+    case 'demandeConnexion':
+    {
+        include("vues/v_connexion.php");
+        break;
+    }
+    case 'valideConnexion':
+    {
+        $login = $_REQUEST['login'];
+        $mdp = $_REQUEST['mdp'];
+        $visiteur = $pdo->getInfosVisiteur($login,$mdp);
+        if(!is_array($visiteur))
+        {
+            ajouterErreur("Login ou mot de passe incorrect");
+            include("vues/v_erreurs.php");
+            include("vues/v_connexion.php");
+        }
+        else
+        {
+            $id = $visiteur['VIS_MATRICULE'];
+            $nom =  $visiteur['VIS_NOM'];
+            $prenom = $visiteur['VIS_PRENOM'];
+            $_SESSION['login']= $login; // mémorise les variables de session
+            $_SESSION['vis_matricule']= $id;
+            $_SESSION['nom']= $nom;
+            $_SESSION['prenom']= $prenom;
+            include("vues/v_sommaire.php");
+            include("vues/v_accueil.php");
+        }
+        break;
+    }
+    case 'deconnexion':
+    {
+        session_destroy();
+        include("vues/v_aurevoir.php");
+        include("vues/v_connexion.php");
+        break;
+    }
+    default :
+    {
+        include("vues/v_connexion.php");
+        break;
+    }
 }
 ?>
