67415913d0ff505162add2d72cb10223a6e6efe8
JeanBaptiste Diaz <diaz.jeanbaptiste@gmail.com>
2013-03-15 09:34:13 +0100

    modif controleur

--- controleurs/c_consulterPraticiens.php
+++ controleurs/c_consulterPraticiens.php
@@ -2,20 +2,4 @@
 include ("vues/v_sommaire.php");
 $patriciens = $pdo->getPraticiens();
 include ("vues/v_lesPraticiens.php");
-
-$action = $_REQUEST['action'];
-
-switch ($action){
-    case 'detail':{
-            $idPatricien = $_REQUEST['idPraticien'];
-            $patriciens = $pdo->UnPatricien($idPatricien);
-            include ("vues/v_detailPatricien.php");
-            break;
-    }
-    case 'tous':{
-            
-            break;
-    }
-}
-
 ?>
