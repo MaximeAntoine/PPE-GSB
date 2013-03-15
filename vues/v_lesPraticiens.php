00d26faa2bbc2109359161858aece8e5df6d5dc2
JeanBaptiste Diaz <diaz.jeanbaptiste@gmail.com>
2013-03-15 09:34:35 +0100

    modif controleur + vue praticien

--- vues/v_lesPraticiens.php
+++ vues/v_lesPraticiens.php
@@ -1,22 +1,28 @@
-
-      <div>
-     <table >
-             <tr>
-                <th >Nom</th>  
-                <th >Prenom</th>               
-             </tr>
-          
-    <?php    
-	   foreach ($patriciens as $unPatriciens) {
-			$nomPatriciens = $unPatriciens['pra_nom'];
-			$prenomPatriciens = $unPatriciens['pra_prenom'];
-		?>		
-            <tr> <td><?php echo $nomPatriciens ?></td>
-                <td><?php echo $prenomPatriciens ?></td> </tr>
-    <?php		          
-          }
-?>	                                          
-    </table>
-      </div>
+<div id="contenu">
+    <table>
+        <tr>
+            <th> Nom </th>
+            <th> Prenom </th>
+            <th> Adresse </th>
+            <th> Ville </th>
+            <th> Coefficient Notoriete </th>
+        </tr>
+        <tr>
+        <?php
+        foreach($patriciens as $unPraticien){
+            $nomP = $unPatricien['pra_nom'];
+            $prenomP = $unPatricien['pra_prenom'];
+            $adrP = $unPatricien['pra_adresse'];
+            $villeP = $unPatricien['pra_ville'];
+            $notP = $unPatricien['pra_coefnotoriete'];
+        
+        ?>
+            <td></td>
+        </tr>
+    
+    <?php 
+        }
+     ?>  
+        </table>
 </div>
 
